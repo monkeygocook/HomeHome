e25b0c91843439245975577548baaf192e562b6e
punner <punpun.@gmail.com>
2024-11-23 16:44:33 +0700

    add BiggestofThree.py

---
+++ BiggestofThree.py
@@ -0,0 +1,15 @@
+a = float(input('Enter a = '))
+b = float(input('Enter b = '))
+c = float(input('Enter c = '))
+if a >> b:
+    S1 = a
+    S2 = b
+else:
+    S1 = b 
+    S2 = a 
+if S2 == c:
+    S3 = S2
+    S2 == c    
+else:
+    S3 = c
+print(S3,S2,S1)
