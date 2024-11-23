b598fec7cb6eaa6f74b611668ee36544600b0457
chonon <kaew@example.com>
2024-11-23 17:16:18 +0700

    It's End

--- BiggestofThree.py
+++ BiggestofThree.py
@@ -1,7 +1,7 @@
 a = float(input('Enter a = '))
 b = float(input('Enter b = '))
 c = float(input('Enter c = '))
-if a >> b:
+if a > b:
     S1 = a
     S2 = b
 else:
@@ -9,7 +9,7 @@ else:
     S2 = a 
 if S2 == c:
     S3 = S2
-    S2 == c    
+    S2 = c    
 else:
     S3 = c
 print(S3,S2,S1)
